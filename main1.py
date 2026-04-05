3c96e66c7471e08985cbd3ede6e816a24c306764
gulchehre_dev <gulchehraakramboeva@gmail.com>
2026-04-05 17:32:05 +0500

    name of commit

--- main1.py
+++ main1.py
@@ -1,16 +1,21 @@
-print("__Добро пожаловать В наш магазин__")
-women_list = ["hello","Dior","Gucci",]
-men_list = ["benaz","irodda"]
+# print("__Добро пожаловать В наш магазин__")
+# women_list = ["hello","Dior","Gucci",]
+# men_list = ["benaz","irodda"]
 
 
-while True:
- women = input("Введите название духов: ")
- if women in women_list:
-        print("Хорошый выбор!")
+# while True:
+#  women = input("Введите название духов: ")
+#  if women in women_list:
+#         print("Хорошый выбор!")
 
- men = input("Введите название духов: ")     
- if men in men_list:  
-    print("Хороший вибор!")
- else:
-    print("Такого парфиума нет!")
+#  men = input("Введите название духов: ")     
+#  if men in men_list:  
+#     print("Хороший вибор!")
+#  else:
+#     print("Такого парфиума нет!")
 
+
+a = "b"
+b = "a"
+
+print(b, a)
