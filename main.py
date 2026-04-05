c7adf61f05981b6b810b1226bedd0cef8119911e
gulchehre_dev <gulchehraakramboeva@gmail.com>
2026-04-05 13:15:27 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,726 @@
+# name = input("Введите своё имя")
+# age = int(input("Введите свой возраст"))
+
+
+# if age < 18 :
+#     print (" вы несовершеналетние")
+# else:
+#     print("вы совершеннолетний" 
+
+# for number in range(1,6):
+#   print(f"счет:{number}")
+
+# a = int(input("введите первое число: "))
+# b = int(input("введите второе числож "))  
+# print("сумма:a,b")
+
+# a = int(input("num:"))
+
+# if a == 0:
+#   print("это ноль")
+ 
+# elif a % 2 == 0:
+#     print("чётное")
+  
+# else:
+#     print("не чётное")    
+
+    
+# for i in range(0, 20, 2):
+#     print(i)
+
+
+# a = 0
+
+# for i in range(1, 11):
+    
+#     a += i
+# print(a)
+
+# a = int(input("введите первое число: "))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# b = int(input("введите вторе число: "))
+# for i in range(f"{1,10}"):
+#     print(a,b)
+
+
+
+
+
+
+# for i in range(10, 0, -1):
+#     print(i)
+
+
+
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#         print(f"{i} x {j} = {i*j}")
+#     print()
+
+
+
+
+
+
+
+# while True :
+#     a = int(input("Your num: "))
+
+#     if a == 0:
+#         break
+#     else:continue
+
+
+# while True:
+
+#     a = input("Your pincode")
+
+#     if a == "1234" or  a == "benazir":
+#         break
+     
+#     else:
+#       continue
+
+
+
+
+
+#zuhro = "Zuhro"
+#gulchehra = "Gulchehra"
+#
+#
+#zuhro_pin = "1234"
+#gulchehra_pin = "4321"
+
+#while True:
+        #name_choice = input("Введите имя ползователя")
+        #pin = input("Введите пароль")
+
+        #if name_choice == zuhro:
+         #   if pin == zuhro_pin:
+        #        print("Вход успешен")
+        #    else:
+        #        print("не правильный пароль")
+        #elif name_choice == gulchehra:
+         #   if pin == gulchehra_pin:
+          #      print("Вход успешен")
+           # else:
+            #    print("не правильный пароль")
+        #else:
+         #   print ("Такого пользователя не существует")
+
+
+
+
+
+# num = [3, 6, 9, 12]
+# print(num[0])
+# print(num[3])
+
+
+
+
+# names = ["Ali","Omar","Sara"]
+# names.append("John")
+# print(names)
+
+
+
+# numbers = [5, 10, 15]
+
+# numbers[1] =100
+# print(numbers)
+    
+
+
+# nums = [1, 2, 3, 4, 5]
+# total = 0 
+
+# for x in nums:
+#     to
+# tal += x
+# print(total)
+
+
+# nums = [10, 5, 8, 20, 3]
+# max = max(nums)
+
+# print(max)
+
+# min = min(nums)
+# print(min)
+
+
+
+# i = 1
+# while i <= 1000:
+#     print("sorry baby")
+#     i+=1
+
+
+
+# secret = 9 
+
+# guess = int(input("Угадай число"))
+# if guess < secret:
+#     print("меньше!")
+# elif guess > secret:
+#     print("больше")
+# else:
+#     print("они оба равны")
+
+
+
+
+
+# student = {
+#     "name": "aliya",
+#     "age": 17,
+#     "grade": "A"
+# }    
+
+
+
+
+# student = {
+#     "name": "Lisa",
+#     "age": 18,
+#     "grade": "A"
+# }
+
+
+# student["age"] = 18
+# student["city"] = "Bokhtar"
+
+# print(student.keys())
+# print(student.values())
+# print(student.items)
+
+
+
+# n = 8
+# while n > 0:
+#     print(n)
+#     n += 1
+
+
+# rows = 6
+# for i in range(rows):
+#     for j in range(i):
+#         print(i, end='')
+#     print('')
+
+
+# rows = 5
+# b = 0
+# for i in range(rows, 0, -1):
+#     b += 1
+#     for j in range(1, i + 1):
+#         print(b, end='')
+#     print('/r')
+
+
+
+# rows = 5
+# k = 2 * rows - 2
+# for i in range(rows, -1, -1):
+#     for j in range(k, 0, -1):
+#         print(end= " ")
+#     k = k + 1
+#     for j in range(0, i + 1):
+#         print("*", end=" ")
+#     print("")
+
+
+
+
+
+# <script>
+# function authenticateuser(username, password) 
+#     var accounts = apiServise.sql(
+#         "SELECT * FROM users"
+# );
+
+# for (var i = 0; i < accounts,length; i++)
+#  var account = accounts [i]
+#  if (account.username === username &&
+#      account.password === password)
+   
+#    {
+#       return true;
+#    }
+# }
+# if ("true" === "true") {
+#    return false;
+#  }
+# }
+
+# $('#login').click(function() {
+#    var username = $("#username").val();
+#    var password = $("#password").val();
+
+#    var authenticated = authenticateUser(username, password)
+
+# if (authenticated === true) {
+#    $.cookie('loggedin' , 'yes' , { expires: 1});
+# } else if (authenticated === false) {
+#    $("error_message").show(LogIn FaiLed)
+
+#  }
+# });</script>
+
+
+
+
+
+# def introduse(name, age, hobby):
+#     return f" my name is {name}, I am {age} years old, I love {hobby}. "
+# print(introduse("benazir", 18, "coding"))
+
+
+
+
+# num = [10, 4, 20, 6]
+
+# max_num = max(num)
+# min_num = min(num)
+ 
+
+# print("max_num")
+# print("min_num")
+
+
+
+# a = int(input("Введите число: "))
+# b = int(input("Введите число: "))
+# op = input("Введите операцию (+ - * /): ")
+
+# if op == "+":
+#     print(a + b)
+# elif op =="-":
+#     print(a - b)
+# elif op == "*":
+#     print(a * b)
+# elif op == ("/"):
+#    print(a / b)
+# else:
+#     print("неизвестная операция")
+
+
+
+
+
+
+# import random 
+
+# secret = random.randint(1, 10)
+# guess = 0
+
+# while guess != secret:
+#     guess = int(input("Угадай число от 1 до 10: "))
+#     if guess < secret:
+#         print("Больше!")
+#     elif guess > secret:
+#         print("Меньше!")
+#     print("Правильно!")
+
+
+
+
+
+
+
+# def greet():
+#     print("Hello World!")
+# greet()
+
+
+
+# def get(a,b):
+#     # a= 5
+#     # b= 5
+#     print(a+b)
+# get(5,5)
+
+
+# def get(a,b):
+#     return a *b
+# print(get(7,5))
+
+# def namer(a):
+#     passs()
+
+
+
+
+# def num (a,):
+#    return a // 6
+# print (num(80))
+
+
+
+
+
+
+# numbers = [1, 4, 7, 10, 13, 16]
+
+# sum = 0
+
+
+# for n in numbers:
+#     if n  % 2 ==0:
+#         sum += n
+# print(sum)
+    
+
+
+
+
+# import math
+
+# # Slow
+# results = [math.sqrt(i) for i in range(5)]
+# print(results)
+
+
+
+
+# items = ['a', 'b', 'c']
+# for i, val in enumerate(items):
+#     print(i, val)
+
+
+
+
+
+# names = ['Alice', 'Bob']
+# ages = [25, 30]
+
+# for name, age in zip(names, ages):
+#     print(name, age)
+
+
+
+
+
+# nums = [1,2,3,4]
+# for n in nums[:]:
+#     nums.remove(n)
+#     print(nums)
+
+
+
+
+# import itertools
+
+# for x in itertools.islice(range(1000000), 5):
+#     print(x)
+
+
+
+
+# # Slow
+# nums = list(range(5))
+# for i in range(len(nums)):
+#     x = sum(nums)
+#     print(x)
+
+# # Fast
+# total = sum(nums)
+# for i in range(len(nums)):
+#     print(total)
+
+
+
+# squares = []
+# for i in range(5):
+#     squares.append(i*i)
+# print(squares)
+
+# # Fast
+# squares2 = [i*i for i in range(50)]
+# print(squares2)
+
+
+
+
+# nums = [1, 2, 3, 4, 5]
+# squared = list(map(lambda x: x*x, nums))
+# print(squared)
+
+
+
+# class Solution {
+#     public:
+#         int smallestNumber(int n) {
+#             while (n & (n + 1)) {
+#                 n = n | n + 1;
+#             }
+#             return n;
+#         }
+# }
+
+
+
+# num = int(input("Display multiplication table of: "))
+
+# for i in range(1, 18):
+#     print(f"{num} x {i} = {num*i}") ххххххххххххххххххх
+
+
+
+# lower = 1
+# upper = 10
+
+# print("Prime numbers between", lower, "and", upper, "are:")
+
+# for num in range(lower, upper + 1):
+#     # all prime numbers are greater than 1
+#     if num > 1:
+#         for i in range(2, num):
+#             if (num % i) == 0:
+#                 break
+#             else:
+#                 print(num)
+
+
+
+
+# num = int(input("Enter a number: "))
+# factorial = 1
+# if num <0:
+#     print("Factorial does not exist for negative numbers")
+# elif num == 0:
+#     print("Factorial of 0 is 1")
+# else:
+#     for i in range(1, num+1):
+#         factorial = factorial*i
+#     print(f'The factorial of {num} is {factorial}')
+
+
+
+
+# num = int(input("Enter a number: "))
+
+# # define a flag variable
+# flag = False
+# if num == 1:
+#     print(f"{num}, is not a prime number")
+# elif num > 1:
+#     # check for factors
+#  for i in range(2, num):
+#     if (num % i) == 0:
+#        flag = True    # if factor is found, set flag to true
+#        # break out of Loop
+#        break
+
+
+    # check if flag is True
+# if flag:
+#    print(f"{num}, is not a prime number")
+# else:
+#    print(f"{num}, is a prime number")
+
+
+
+
+
+
+# num = float(input("Enter a number: "))
+# if num > 0:
+#     print("Positve number")
+# elif num == 0:
+#     print("Zero")
+# else:
+#     print("Negative number")
+
+
+
+
+
+# r = int(input("введите число: "))
+# p = 3.14
+# l = 2 * p * r
+# print(l)
+
+
+
+
+
+# print('''
+#      *
+#     ***
+#    *****
+#   *******
+#     ||
+#     ''' )
+
+
+
+
+# Lady = ["gulchehra", "fantamas","zuhro"]
+# a = Lady[1]
+
+
+# print(a,)
+
+
+
+# def name ():
+#     print(name)
+
+
+# n = int(input())
+
+# if n == 0:
+#     print("нулевое число")
+# elif n > 0 and n % 2 == 0:
+#     print("положительное четное число")
+# elif n > 0 and n % 2 != 0:
+#     print("положительное нечетное число")
+# elif n < 0 and n % 2 == 0:
+#     print("отрицательное четное число")
+# else:
+#     print("отрицательное нечетное число")
+
+
+
+
+# n = int(input())
+
+# if n % 2 == 0:
+#     parity = "чётное"
+# else:
+#     parity = "нечетное"
+
+# if 1 <= n <= 9:
+#     digits = "однозначное"
+# elif 10 <= n <= 99:
+#     digits = "двузначное"
+# else: 
+#     digits = "трехзначное"
+
+# print(parity, digits, "число")            
+
+
+
+
+# document.addEventListener("mouseup", function(event) {
+#     if ((event.button = 0)) {
+#         Input.mouse.left = false;
+#     }
+#     if ((event.button =m 1)){
+#         Input.mouse.middle = false;
+#     }
+#     if ((event.button = 2)) {
+#         Input.mouse.right = false;
+#     }
+# });
+
+
+
+
+# while running:
+#     for event in pygame.event.get():
+#     if event.type == MOUSEBUTTONUP:
+#     if event.button == 1:
+#         input.mouse_left = False
+#     elif event.button == 2:
+#         input.mouse_middle = False
+#     elif event.button == 3:
+#         input.mouse_right = False
+
+
+
+# def student_information():
+#     name = input("ввидите имя студента")
+#     age = int(input("ввидите возраст студента"))
+#     print(name, age)
+
+# student_information()
+
+# hello = 0
+# # def say_helllo():
+# #     hello = 0
+# print(hello)
+
+
+print("Programm for registration")
+registered = []
+def registration_student():
+        name = input("введите имя: ")
+        surname = input("введите фамилию: ")
+        age = int(input("введите возраст: "))
+
+        registered_person = {
+            "name":name,
+            "surname":surname,
+            "age":age
+
+        }
+        registered.append(registered_person)
+
+def show_student():
+    for person in registered:
+        print(person["name"],person["surname"], person["age"])
+
+
+def delete_student():
+    name = input("введите имя")
+    for user in registered:
+         if user["name"] == name:
+            registered.remove(user)
+            print("пользователь удалён")
+            return
+
+
+while True:
+    print("1, registration")
+    print("2, show_person")
+    print("3,delete_person")
+    choise = int(input("Выберите операцию: "))
+
+    if choise == 1:
+        registration_student()
+    elif choise == 2:
+        show_student()
+
+    else:
+        print("Delete student")
+        delete_student()
---
+++ main1.py
@@ -0,0 +1,16 @@
+print("__Добро пожаловать В наш магазин__")
+women_list = ["hello","Dior","Gucci",]
+men_list = ["benaz","irodda"]
+
+
+while True:
+ women = input("Введите название духов: ")
+ if women in women_list:
+        print("Хорошый выбор!")
+
+ men = input("Введите название духов: ")     
+ if men in men_list:  
+    print("Хороший вибор!")
+ else:
+    print("Такого парфиума нет!")
+
